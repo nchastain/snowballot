a690080bfd7a9ee6bb63677aded276704a874803
Nate Chastain <chastain616@gmail.com>
2017-03-24 18:18:34 -0400

    Init commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+var Main = (props) => {
+  return (
+    <div>
+      <div className="row">
+        <div className="column small-centered medium-6 large-4">
+        <h1>React Boilerplate</h1>
+          {props.children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+module.exports = Main;
