952a32534a37e44ec04d880e51a449a4957221fd
Nate Chastain <chastain616@gmail.com>
2017-04-23 20:44:38 -0400

    expiration dates on sbs in place

--- app/components/SbDetail.jsx
+++ app/components/SbDetail.jsx
@@ -5,6 +5,7 @@ import FA from 'react-fontawesome'
 import * as actions from '.././actions'
 import Redux from 'redux'
 import moment from 'moment'
+import classnames from 'classnames'
 
 let createHandlers = function (dispatch) {
   let updateSb = function (id, choiceId, updatedSb, fresh) {
@@ -31,11 +32,15 @@ export class SbDetail extends Component {
     super(props)
     let voted = this.props.sb.userChoice
     let votedChoiceId = this.props.sb.userVoted
+    let expired = false
+    if (typeof this.props.sb.expires === 'string') expired = moment(new Date(this.props.sb.expires)).isBefore(moment(Date.now()))
     this.state = {
       voted: voted,
-      votedChoiceId: votedChoiceId
+      votedChoiceId: votedChoiceId,
+      expired: expired
     }
     this.handlers = createHandlers(this.props.dispatch)
+    this.sbClasses = this.sbClasses.bind(this)
   }
 
   componentDidMount () {
@@ -43,16 +48,25 @@ export class SbDetail extends Component {
   }
 
   componentWillReceiveProps (nextProps) {
+    let expired = false
+    if (nextProps.sb) {
+      expired = nextProps.sb.expires && moment(new Date(this.props.sb.expires)).isBefore(moment(Date.now()))
+    }
     if (nextProps.sb && nextProps.sb.userVoted && nextProps.sb.userChoice) {
       let voted = nextProps.sb.userVoted
       let votedChoiceId = nextProps.sb.userChoice
-      this.setState({voted: voted, votedChoiceId: votedChoiceId})
+      this.setState({voted: voted, votedChoiceId: votedChoiceId, expired: expired})
     }
   }
 
   vote (e, choiceId) {
     let oldDOMRefs = setDOMReferences(e)
     const {selected, plusText, voteCount} = oldDOMRefs
+    let expired = this.props.sb.expires && moment(new Date(this.props.sb.expires)).isBefore(moment(Date.now()))
+    if (expired) {
+      this.setState({expired: true})
+      return
+    }
     if (!selected) {
       e.currentTarget.querySelector('.selection-icon.selected').style.display = 'inline-block'
       e.currentTarget.querySelector('.selection-icon.unselected').style.display = 'none'
@@ -93,6 +107,15 @@ export class SbDetail extends Component {
     this.setState({voted: freshVote, votedChoiceId: freshVote ? choiceId : ''})
   }
 
+  sbClasses (choice) {
+    return classnames({
+      'box-header': true,
+      'clearfix': true,
+      'selected': !this.state.expired && choice.id === this.props.sb.userChoice,
+      'leader': this.state.expired && choice.id === this.findLeader(this.props.sb.choices).id
+    })
+  }
+
   renderSelectedChoice (choice) {
     return choice.id === this.state.votedChoiceId
     ? <span>
@@ -121,37 +144,27 @@ export class SbDetail extends Component {
       </span>
   }
 
-  renderCreatorMessage () {
-    return moment.unix(this.props.sb.createdAt).subtract(1, 'hours').isBefore(moment().unix())
-    ? <div className='creator-message'>
-        <span className='creator-message-text'>
-        You just created this snowballot! Now <a href={`/sbs/${this.props.sb.alias}`}>send it to people</a> and bask in the wisdom of crowds.
-        </span>
-      </div>
-    : <div className='creator-message'>
-      <span className='creator-message-text'>
-      You created this snowballot on {moment.unix(this.props.sb.createdAt).format('dddd, MMMM Do, YYYY')}
-      </span>
-      </div>
-  }
-
   hoverVote (e) {
-    if (!this.props.user.uid) return
+    if (!this.props.user.uid || this.state.expired) return
     let DOMRefs = setDOMReferences(e)
-    const {plusText, voteCount} = DOMRefs
+    const {selected, plusText, voteCount} = DOMRefs
     const offset = 10
     const show = `-${offset}px`
     const hide = `-${offset + 25}px`
     const currentVoteCount = e.currentTarget.querySelector('.vote-count')
     const currentPlus = e.currentTarget.querySelector('.plus')
+    const currentContainer = e.currentTarget
     if (e.type === 'mouseenter') {
       currentVoteCount.style.marginRight = show
       currentPlus.style.visibility = 'visible'
       currentPlus.style.opacity = '1'
+      currentContainer.style.cursor = 'pointer'
+      currentContainer.style.backgroundColor = '#bfe6ff'
     } else if (e.type === 'mouseleave') {
       currentVoteCount.style.marginRight = hide
       currentPlus.style.visibility = 'hidden'
       currentPlus.style.opacity = '0'
+      currentContainer.style.backgroundColor = selected ? '#bfe6ff' : 'white'
     }
     if (plusText && voteCount) {
       if (e.type === 'mouseenter') {
@@ -166,6 +179,32 @@ export class SbDetail extends Component {
     }
   }
 
+  findLeader (choices) {
+    let most = 0
+    let leader = null
+    choices.forEach(function (choice) {
+      if (choice.votes > most) {
+        most = choice.votes
+        leader = choice
+      }
+    })
+    return leader
+  }
+
+  renderCreatorMessage () {
+    return moment.unix(this.props.sb.createdAt).subtract(1, 'hours').isBefore(moment().unix())
+    ? <div className='creator-message'>
+        <span className='creator-message-text'>
+        You just created this snowballot! Now <a href={`/sbs/${this.props.sb.alias}`}>send it to people</a> and bask in the wisdom of crowds.
+        </span>
+      </div>
+    : <div className='creator-message'>
+      <span className='creator-message-text'>
+      You created this snowballot on {moment.unix(this.props.sb.createdAt).format('dddd, MMMM Do, YYYY')}
+      </span>
+      </div>
+  }
+
   renderAuthMessage () {
     return this.props.user.uid
     ? null
@@ -174,6 +213,14 @@ export class SbDetail extends Component {
         <span className='login-message-text'>
         To vote on this, <Link to='/login'>log</Link> in or <Link to='/register'>register</Link>. It's how we stop stage moms from rigging children's beauty pageants.</span>
       </div>
+  }
+
+  renderExpiresMessage () {
+    return (
+      <span className='expiration-info'>
+        <FA name='clock-o' className='fa fa-fw' />This snowballot {this.state.expired ? 'has expired.' : `expires after ${this.props.sb.expires}` }
+      </span>
+    )
   }
 
   renderSb () {
@@ -185,7 +232,7 @@ export class SbDetail extends Component {
           {this.props.sb.choices.map((choice, idx) =>
             <div
               key={choice.title + idx}
-              className={this.props.user.uid ? choice.id === this.props.sb.userChoice ? 'box-header clearfix selected' : 'box-header clearfix' : 'box-header clearfix static'}
+              className={this.sbClasses(choice)}
               onClick={(e) => !this.props.user.uid ? null : this.vote(e, choice.id)}
               onMouseEnter={(e) => this.hoverVote(e)}
               onMouseLeave={(e) => this.hoverVote(e)}
@@ -193,6 +240,7 @@ export class SbDetail extends Component {
               <div className='left-cell'>
                 <FA className='fa-fw selection-icon unselected' name='circle-o' />
                 <FA className='fa-fw selection-icon selected' name='check-circle' />
+                <FA className='fa-fw selection-icon trophy' name='trophy' />
               </div>
               <div className='right-cell'>
                 <li key={this.props.sb.id + choice.title + idx}>{choice.title}</li>
@@ -210,7 +258,8 @@ export class SbDetail extends Component {
   render () {
     return (
       <div>
-        <div>
+        <div className='above-sb-container'>
+          {this.props.sb.expires && this.renderExpiresMessage()}
           {this.props.user.uid === this.props.sb.creator && this.renderCreatorMessage()}
           {this.renderAuthMessage()}
         </div>
